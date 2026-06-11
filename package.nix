ca3da72d64a94e5660c84723729f662bb612db81
JustinQM <jumpingbeanlittlefoot@gmail.com>
2026-06-11 10:53:16 -0700

    fixed the packages root not getting stripped which was messing the hash up

--- package.nix
+++ package.nix
@@ -1,26 +1,25 @@
-{ lib
-, stdenv
-, fetchzip
-, buildFHSEnv
-, qt6
-, libX11
-, libxcb
-, xorg
-, gcc-unwrapped
-, glib
-, zlib
-, makeWrapper
+{ 
+    lib, 
+    stdenv, 
+    fetchzip, 
+    buildFHSEnv, 
+    qt6, 
+    libX11, 
+    libxcb, 
+    xorg, 
+    gcc-unwrapped, 
+    glib, 
+    zlib, 
+    makeWrapper
 }:
 
 let
   version = "7.7";
 
-  bundle = fetchzip {
-    url = "https://cheatengine.org/download/CheatEngineLinux77.zip";
-    # populated by update.sh
-    hash = "sha256-6eYwxx/vGqoJdzz3nQVTB7GjsEb0zOG861y9YbkLxe8=";
-    stripRoot = false;
-  };
+    bundle = fetchzip {
+      url = "https://cheatengine.org/download/CheatEngineLinux77.zip";
+      hash = "sha256-DTaP/bCy1Q4nURa0Elh4i1rjjvKyQevzUp+gVw8LQas=";
+    };
 
   # Inner derivation: just unpacks and installs the bundle as-is
   ce-unwrapped = stdenv.mkDerivation {
@@ -40,18 +39,19 @@ let
 in buildFHSEnv {
   name = "cheat-engine";
 
-  targetPkgs = pkgs: with pkgs; [
-    qt6.qtbase
-    qt6.qtwayland
-    libX11
-    libxcb
-    xorg.libXcursor
-    xorg.libXi
-    xorg.libXrandr
-    gcc-unwrapped.lib   # libstdc++.so.6
-    glib
-    zlib
-  ];
+    targetPkgs = pkgs: with pkgs; 
+    [
+        qt6.qtbase
+        qt6.qtwayland
+        libX11
+        libxcb
+        libxcursor   
+        libxi        
+        libxrandr    
+        gcc-unwrapped.lib
+        glib
+        zlib
+    ];
 
   runScript = ''
     cd ${ce-unwrapped}/share/cheat-engine
